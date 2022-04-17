8512262ea784fe5527becab301c9db85cdfe48df
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2022-04-17 22:45:52 +0300

    Created camera and renderer objects for the scene

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.module.js';
 
-const scene = new THREE.Scene();
-console.log(scene);
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer()
+
+//just for testing
+console.log(scene)
+console.log(camera)
+console.log(renderer)
+
+renderer.setSize(innerWidth, innerHeight)
+document.body.appendChild(renderer.domElement)

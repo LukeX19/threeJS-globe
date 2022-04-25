14982c0fa298de82c88379dc43d90b45054e84ff
Cătălin Secoșan <secosan.catalinluca@gmail.com>
2022-04-26 01:37:22 +0300

    Added permanent static rotation and normalized direction of vertices

--- shaders/atmosphere_vertex.glsl
+++ shaders/atmosphere_vertex.glsl
@@ -3,7 +3,7 @@ varying vec3 vertexNormal;
 
 void main()
 {
-    vertexNormal = normal;
+    vertexNormal = normalize(normalMatrix * normal);
     //for every vertex, set its position (xyz coordinates)
     //fourth argument is w (used for translates/transforms)
     //position is of type 3, so it has the (1, 0, 1) value
